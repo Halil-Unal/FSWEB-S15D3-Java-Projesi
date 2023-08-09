ce115ab912eb8513582fb5d6fbaa6a7d5bda0ba5
HALİL ÜNAL <125642299+Halil-Unal@users.noreply.github.com>
2023-08-09 18:03:50 +0300

    ff

--- src/com/workintech/company/main/Main.java
+++ src/com/workintech/company/main/Main.java
@@ -6,19 +6,20 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<Employee> employees = new LinkedList<>();
+List<Employee> employees=new LinkedList<>();
         employees.add(new Employee(1, "Dogancan", "Kınık"));
         employees.add(new Employee(1, "Dogancan", "Kınık"));
         employees.add(new Employee(2, "Serkan", "Toraman"));
         employees.add(new Employee(2, "Serkan", "Toraman"));
         employees.add(new Employee(3, "Ilgın", "Oktem"));
         employees.add(new Employee(3, "Ilgın", "Oktem"));
-        //System.out.println(employees);
 
-        Map<Integer, Employee> employeeMap = new HashMap<>();
-        List<Employee> removedEmployees = new ArrayList<>();
+
+Map<Integer,Employee> employeeMap=new HashMap<>();
+List<Employee> removedEmployees=new ArrayList<>();
 
         Iterator iterator = employees.iterator();
+
         while (iterator.hasNext()){
             Employee employee = (Employee) iterator.next();
             if(employeeMap.containsKey(employee.getId())){
@@ -29,14 +30,8 @@ public class Main {
             }
         }
 
-        //System.out.println(removedEmployees);
-//        for(Employee employee: removedEmployees){
-//            employees.remove(employee);
-//        }
         System.out.println(employees);
         System.out.println(employeeMap);
-        //employees.removeAll(removedEmployees);
 
-        //System.out.println(employees);
     }
 }
